f7d5a774c0afd9d2bee055cc29d99402a93a0c84
Diogo Moitinho de Almeida <diogo149@gmail.com>
2016-07-04 12:38:59 -0700

    add dropout to mlp

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -13,17 +13,21 @@ sess = tf.InteractiveSession()
 x = tf.placeholder(tf.float32, shape=[None, 1, 28, 28])
 y_ = tf.placeholder(tf.int64, shape=[None])
 
-l = tfu.flatten(x, 2)
+keep_prob = tf.placeholder(tf.float32)
+
+h = tfu.flatten(x, 2)
 
 with tf.variable_scope("mlp",
                        initializer=tf.random_uniform_initializer(-0.05, 0.05)):
-    l = tfu.affine("fc1", l, 256)
-    l = tf.nn.relu(l)
-    l = tfu.affine("fc2", l, 256)
-    l = tf.nn.relu(l)
-    l = tfu.affine("logit", l, 10)
+    h = tfu.affine("fc1", h, 256)
+    h = tf.nn.relu(h)
+    h = tf.nn.dropout(h, keep_prob=keep_prob)
+    h = tfu.affine("fc2", h, 256)
+    h = tf.nn.relu(h)
+    h = tf.nn.dropout(h, keep_prob=keep_prob)
+    h = tfu.affine("logit", h, 10)
 
-cross_entropy = tf.reduce_mean(tfu.softmax_cross_entropy_with_logits(l, y_))
+cross_entropy = tf.reduce_mean(tfu.softmax_cross_entropy_with_logits(h, y_))
 
 # train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
@@ -40,7 +44,7 @@ def to_minibatches(dataset, batch_size):
         res = {k: np.array(v) for k, v in res.items()}
         yield res
 
-accuracy = tf.reduce_mean(tfu.categorical_accuracy(l, y_))
+accuracy = tf.reduce_mean(tfu.categorical_accuracy(h, y_))
 
 sess.run(tf.initialize_all_variables())
 
@@ -48,5 +52,9 @@ train_gen = to_minibatches(train, 50)
 for _ in range(10):
     for i in range(1000):
         batch = train_gen.next()
-        train_step.run(feed_dict={x: batch["x"], y_: batch["y"]})
-    print(accuracy.eval(feed_dict={x: valid["x"], y_: valid["y"]}))
+        train_step.run(feed_dict={x: batch["x"],
+                                  y_: batch["y"],
+                                  keep_prob: 0.5})
+    print(accuracy.eval(feed_dict={x: valid["x"],
+                                   y_: valid["y"],
+                                   keep_prob: 1.0}))
